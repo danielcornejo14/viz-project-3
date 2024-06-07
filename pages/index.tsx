2d421d3b2e249f3bc36f93776f449cfd50efe0bb
danicor1401 <danicor1401@gmail.com>
2024-06-07 00:15:39 +0000

    feat: all visualizations

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
 import { Inter } from "next/font/google";
 import DiseasesSimulation from "./components/DiseasesSimulations";
+import EurosisSimulation from "./components/EurosisSimulation";
+import SchoolSimulation from "./components/SchoolSimulation";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -7,6 +9,8 @@ export default function Home() {
   return (
     <div>
       <DiseasesSimulation></DiseasesSimulation>
+      <EurosisSimulation></EurosisSimulation>
+      <SchoolSimulation></SchoolSimulation>
     </div>
   );
 }
